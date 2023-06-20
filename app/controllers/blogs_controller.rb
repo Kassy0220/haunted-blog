f0ec778a51b4750cc91cdf74fd307120627d5c7b
github Kassy <github@shinbokutaro0220@gmail.com>
2023-06-20 18:53:49 +0900

    アイキャッチ機能を適切なユーザが使用できるように、Strong Parameters を修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,7 +20,6 @@ class BlogsController < ApplicationController
 
   def create
     @blog = current_user.blogs.new(blog_params)
-    redirect_to new_blog_url and return if !@blog.user.premium && @blog.random_eyecatch
 
     if @blog.save
       redirect_to blog_url(@blog), notice: 'Blog was successfully created.'
@@ -30,8 +29,6 @@ class BlogsController < ApplicationController
   end
 
   def update
-    redirect_to edit_blog_url and return if !@blog.user.premium && params[:blog][:random_eyecatch]
-
     if @blog.update(blog_params)
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
@@ -57,6 +54,9 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    permitted_attributes = %i[title content secret]
+    permitted_attributes.push(:random_eyecatch) if current_user.premium
+
+    params.require(:blog).permit(*permitted_attributes)
   end
 end
